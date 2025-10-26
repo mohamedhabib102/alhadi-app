1b2d53b1f75a2190e179055d14417fb1a6f2de5b
Mohamed Habib <byaalkhy43@gmail.com>
2025-10-26 23:50:30 +0300

    other testing...

--- components/ui/CardAbout.tsx
+++ components/ui/CardAbout.tsx
@@ -18,11 +18,16 @@ const CardAbout: React.FC<CardList> = ({ data }) => {
         <motion.div
           key={ele.id}
           className="relative overflow-hidden rounded-2xl shadow-md cursor-pointer transition-all duration-500 hover:shadow-2xl"
-           initial={{ y: 60, opacity: 0 }}
-           whileInView={{ y: 0, opacity: 1 }}
-           viewport={{ once: true, amount: 0.3 }}
-           transition={{ duration: 0.8, ease: [0.16, 1, 0.3, 1] }}          >
-          <div className="overflow-hidden">
+          initial={{ y: 60, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          viewport={{ once: true, amount: 0.3 }}
+          transition={{ duration: 0.8, ease: [0.16, 1, 0.3, 1] }}
+        >
+          <motion.div
+            className="overflow-hidden"
+            whileTap={{ scale: 1.05, filter: "grayscale(0) brightness(1.1)" }}
+            transition={{ duration: 0.7, ease: [0.16, 1, 0.3, 1] }}
+          >
             <Image
               src={ele.image}
               title="معكم قلوبًا راحمة"
@@ -31,7 +36,7 @@ const CardAbout: React.FC<CardList> = ({ data }) => {
               height={400}
               className="w-full h-auto object-cover transition-all duration-700 ease-in-out filter grayscale brightness-90 hover:grayscale-0 hover:brightness-110 hover:scale-105"
             />
-          </div>
+          </motion.div>
         </motion.div>
       ))}
     </>
