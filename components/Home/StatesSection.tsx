05e37ceadd236970a33dc9865d51e60084da3690
Mohamed Habib <byaalkhy43@gmail.com>
2025-10-30 19:37:05 +0300

    edit in home

--- components/Home/StatesSection.tsx
+++ components/Home/StatesSection.tsx
@@ -58,7 +58,7 @@ const StatsSection: React.FC = () => {
                     أرقامنا تترجم إلى أمل وكل إحصائية قصة حياة نغيرها للأفضل
                 </motion.h2>
 
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-8">
                    {sections.slice(0, 5).map((item, index) => (
                      <motion.div
                        key={item.sectionID}
